1cd721605c9eef1bcea5fd605fd976368101f63a
ServerBBQ <76498501+ServerBBQ@users.noreply.github.com>
2025-01-07 16:52:58 +0000

    fix a few  bugs and add keypresses to include map

--- keymap/step2_csv_to_javelin-script.py
+++ keymap/step2_csv_to_javelin-script.py
@@ -26,7 +26,10 @@ def javelin_keystring_template(row):
 
 def convertTextToKeypress(text):
   # List of scan codes https://github.com/jthlim/javelin-steno/blob/main/script.md#constants-1
+  if text == '"': text = '\\"' # Escape the double quote
+  if text == '\\': text = '\\\\' # Escape the slash
   keypress = rf'sendText("{text}");'
+
   # Modifiers, Arrows keys, Page up, page down, end & home already implemented within the script itself, no need to include those there
   if text.lower() == "tab": keypress = r"tapScanCode(0x2b); // tab" # SC_TAB
   if text.lower() == "space": keypress = r"tapScanCode(0x2c); // space" # SC_SPACE
@@ -35,18 +38,18 @@ def convertTextToKeypress(text):
   if text.lower() == "del": keypress = r"tapScanCode(0x4c); // del" # SC_DELETE
   if text.lower() == "ins": keypress = r"tapScanCode(0x48); // ins" # SC_INSERT
   if text.lower() == "backspace": keypress = r"tapScanCode(0x2a); // backspace" # SC_BACKSPACE
-  if text.lower() == "f1": keypress = f"tapScanCode(0x3a);" # 
-  if text.lower() == "f2": keypress = f"tapScanCode(0x3b);" # 
-  if text.lower() == "f3": keypress = f"tapScanCode(0x3c);" # 
-  if text.lower() == "f4": keypress = f"tapScanCode(0x3d);" # 
-  if text.lower() == "f5": keypress = f"tapScanCode(0x3e);" # 
-  if text.lower() == "f6": keypress = f"tapScanCode(0x3f);" # 
-  if text.lower() == "f7": keypress = f"tapScanCode(0x40);" # 
-  if text.lower() == "f8": keypress = f"tapScanCode(0x41);" # 
-  if text.lower() == "f9": keypress = f"tapScanCode(0x42);" # 
-  if text.lower() == "f10": keypress = f"tapScanCode(0x43)" # 
-  if text.lower() == "f11": keypress = f"tapScanCode(0x44)" # 
-  if text.lower() == "f12": keypress = f"tapScanCode(0x45)" # 
+  if text.lower() == "f1": keypress = f"tapScanCode(0x3a); // f1" # 
+  if text.lower() == "f2": keypress = f"tapScanCode(0x3b); // f2" # 
+  if text.lower() == "f3": keypress = f"tapScanCode(0x3c); // f3" # 
+  if text.lower() == "f4": keypress = f"tapScanCode(0x3d); // f4" # 
+  if text.lower() == "f5": keypress = f"tapScanCode(0x3e); // f5" # 
+  if text.lower() == "f6": keypress = f"tapScanCode(0x3f); // f6" # 
+  if text.lower() == "f7": keypress = f"tapScanCode(0x40); // f7" # 
+  if text.lower() == "f8": keypress = f"tapScanCode(0x41); // f8" # 
+  if text.lower() == "f9": keypress = f"tapScanCode(0x42); // f9" # 
+  if text.lower() == "f10": keypress = f"tapScanCode(0x43) // f10" # 
+  if text.lower() == "f11": keypress = f"tapScanCode(0x44) // f11" # 
+  if text.lower() == "f12": keypress = f"tapScanCode(0x45) // f12" # 
   if text.lower() == "play/pause": keypress = r"tapScanCode(0xbd); // play pause" # SC_CONSUMER_PLAY_PAUSE
   if text.lower() == "volume up": keypress = r"tapScanCode(0x80); // vol up" # SC_VOLUME_UP 
   if text.lower() == "volume down": keypress = r"tapScanCode(0x81); // vol down" # SC_VOLUME_DOWN 
@@ -60,13 +63,13 @@ def convertTextToKeypress(text):
 
   # Special
   #                                              press ctrl          press X               release X          release ctrl
-  if text.lower() == "cut": keypress = r"pressScanCode(0xe0);pressScanCode(0x1b);releaseScanCode(0x1b);releaseScanCode(0xe0); // cut"
+  if text.lower() == "cut": keypress = r"{pressScanCode(0xe0);pressScanCode(0x1b);releaseScanCode(0x1b);releaseScanCode(0xe0);} // cut"
   #                                               press ctrl          press C               release C          release ctrl
-  if text.lower() == "copy": keypress = r"pressScanCode(0xe0);pressScanCode(0x06);releaseScanCode(0x06);releaseScanCode(0xe0); // copy"
+  if text.lower() == "copy": keypress = r"{pressScanCode(0xe0);pressScanCode(0x06);releaseScanCode(0x06);releaseScanCode(0xe0);} // copy"
   #                                               press ctrl          press V               release V          release ctrl
-  if text.lower() == "paste": keypress = r"pressScanCode(0xe0);pressScanCode(0x19);releaseScanCode(0x19);releaseScanCode(0xe0); // paste"
+  if text.lower() == "paste": keypress = r"{pressScanCode(0xe0);pressScanCode(0x19);releaseScanCode(0x19);releaseScanCode(0xe0);} // paste"
   #                                               press ctrl          press Z               release Z          release ctrl
-  if text.lower() == "undo": keypress = r"pressScanCode(0xe0);pressScanCode(0x1d);releaseScanCode(0x1d);releaseScanCode(0xe0); // undo"
+  if text.lower() == "undo": keypress = r"{pressScanCode(0xe0);pressScanCode(0x1d);releaseScanCode(0x1d);releaseScanCode(0xe0);} // undo"
   
   return keypress
 # %%
@@ -83,4 +86,14 @@ for key in key_list:
     df[key] = df[key].replace(1, key)
 df["taipoKeyString"] = df[key_list].agg(''.join, axis=1)
 df = df.apply(javelin_keystring_template, axis=1)
-df.to_csv("keystring_list.javelin-script", index=False, quoting=csv.QUOTE_NONE, escapechar="\\")
+df.to_csv("keystring_list.javelin-script", index=False, quoting=csv.QUOTE_NONE, escapechar="😂") # I don't want it to try to escape anything
+
+# Remove the first line which contains a zero
+with open("keystring_list.javelin-script", "r") as file:
+    lines = file.readlines()
+
+# Remove the 😂 character and remove first character
+processed_lines = [line.replace("😂", "") for line in lines[1:]]
+
+with open("keystring_list.javelin-script", "w") as file:
+    file.writelines(processed_lines)
