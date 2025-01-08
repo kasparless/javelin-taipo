3013995a1bc27dd0879758a5f49513ff25a0b7b8
kasparless <165455886+kasparless@users.noreply.github.com>
2025-01-08 17:56:37 +1000

    first javelin build attempt

---
+++ keymap/generate_key_trigger_scripts.py
@@ -0,0 +1,11 @@
+# %%
+key_list = ["R","S","N","I","A","O","T","E","Inner","Outer"]
+side_list = [("left",0),("right",1)]
+action_list = ["press","release"]
+# %%
+with open("temp.txt","w") as f:
+    for side_txt, side_val in side_list:
+        f.writelines(side_txt+"\n")
+        for key in key_list:
+            for action in action_list:
+                f.writelines(f"{action}Taipo{key}({side_val});"+"\n")
